0501a364b6f5098feb6d88bca546459a9413f33a
GreatGameDota <17858650+GreatGameDota@users.noreply.github.com>
2019-07-09 20:18:26 -0500

    Reupload files
    Upload correct version of main files

---
+++ Player.java
@@ -0,0 +1,78 @@
+/**
+ * @author GreatGameDota
+ */
+
+public class Player {
+	private int globalMoney;
+	private int money;
+	private String name;
+
+	public Player(String name) {
+		globalMoney = 0;
+		money = 0;
+		this.name = name;
+	}
+
+	/**
+	 * Description: Adds m to player's money
+	 * 
+	 * @param int m
+	 */
+	public void addMoney(int m) {
+		money += m;
+	}
+
+	/**
+	 * Description: Subtracts m from player's money
+	 * 
+	 * @param int m
+	 */
+	public void deductMoney(int m) {
+		money -= m;
+	}
+
+	/**
+	 * @return the money
+	 */
+	public int getMoney() {
+		return money;
+	}
+
+	/**
+	 * @param money
+	 *            the money to set
+	 */
+	public void setMoney(int money) {
+		this.money = money;
+	}
+
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+
+	/**
+	 * @param name
+	 *            the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	/**
+	 * @return the globalMoney
+	 */
+	public int getGlobalMoney() {
+		return globalMoney;
+	}
+
+	/**
+	 * @param globalMoney
+	 *            the globalMoney to set
+	 */
+	public void setGlobalMoney(int globalMoney) {
+		this.globalMoney = globalMoney;
+	}
+}
